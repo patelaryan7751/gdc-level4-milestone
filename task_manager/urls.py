381a4d63b68f66e24a11cd55cdf39422391656ca
ARYAN PATEL <patelaryan7751@gmail.com>
2022-02-03 14:28:16 +0530

    optimized the code
    moved all views from url.py to views.py

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,58 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from django.http import HttpResponse, HttpResponseRedirect
-
-from django.shortcuts import render
-
-tasks = []
-completedTasks = []
-
-
-def deleteTask(index):
-    del tasks[index - 1]
-
-
-def completeTask(index):
-    completedTasks.append(tasks[index - 1])
-
-
-def tasks_view(request):
-    return render(request, "tasks.html", {"tasks": tasks, "completedTasks": completedTasks})
-
-
-def home_view(request):
-    return render(request, "index.html", {"tasks": tasks, "completedTasks": completedTasks})
-
-
-def completed_tasks_view(request):
-    return render(request, "completedTasks.html", {"tasks": tasks, "completedTasks": completedTasks})
-
-
-def all_tasks_view(request):
-    return render(request, "allTasks.html", {"tasks": tasks, "completedTasks": completedTasks})
-
-
-def add_task_view(request):
-    task_value = request.GET.get("task")
-    tasks.append(task_value)
-    return HttpResponseRedirect("/tasks")
-
-
-def delete_task_view(request, index):
-    deleteTask(index)
-    return HttpResponseRedirect("/tasks")
-
-
-def complete_task_view(request, index):
-    completeTask(index)
-    deleteTask(index)
-    return HttpResponseRedirect("/tasks")
+from tasks.views import tasks_view, add_task_view, delete_task_view, home_view, completed_tasks_view, complete_task_view, all_tasks_view
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     # Add all your views here
-    path("manage/", admin.site.urls),
     path("", home_view),
     path("tasks/", tasks_view),
     path("add-task/", add_task_view),
--- tasks/views.py
+++ tasks/views.py
@@ -1,1 +1,47 @@
 # Add all your views here
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+
+tasks = []
+completedTasks = []
+
+
+def deleteTask(index):
+    del tasks[index - 1]
+
+
+def completeTask(index):
+    completedTasks.append(tasks[index - 1])
+
+
+def tasks_view(request):
+    return render(request, "tasks.html", {"tasks": tasks, "completedTasks": completedTasks})
+
+
+def home_view(request):
+    return render(request, "index.html", {"tasks": tasks, "completedTasks": completedTasks})
+
+
+def completed_tasks_view(request):
+    return render(request, "completedTasks.html", {"tasks": tasks, "completedTasks": completedTasks})
+
+
+def all_tasks_view(request):
+    return render(request, "allTasks.html", {"tasks": tasks, "completedTasks": completedTasks})
+
+
+def add_task_view(request):
+    task_value = request.GET.get("task")
+    tasks.append(task_value)
+    return HttpResponseRedirect("/tasks")
+
+
+def delete_task_view(request, index):
+    deleteTask(index)
+    return HttpResponseRedirect("/tasks")
+
+
+def complete_task_view(request, index):
+    completeTask(index)
+    deleteTask(index)
+    return HttpResponseRedirect("/tasks")
